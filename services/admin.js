9f5a31ea9d91cd13df25de2e97443dad7e1b051d
PeculiarE <75625011+PeculiarE@users.noreply.github.com>
2021-04-02 07:15:12 +0100

    Update admin.js

--- services/admin.js
+++ services/admin.js
@@ -2,7 +2,7 @@ import db from '../db/setup';
 import {
   getAdminByEmail, insertNewApplication, getBatchId, getCurrentBatch, insertQuestions,
   insertAssessmentDetails, fetchAllUsers, updateUserStatusbyEmail, updateAdmin,
-  insertAssessmentQuestions, getOneQuestion, updateApprovalStatus,
+  insertAssessmentQuestions, getOneQuestion, updateApprovalStatus, getAllApplicantsByBatchId,
 } from '../db/queries/admin';
 import { generateUUID } from '../utils';
 
@@ -97,3 +97,5 @@ export const updateAdminDetails = async (adminId, data) => {
 export const updateUserApprovalStatus = async (userId, data) => (
   db.none(updateApprovalStatus, [userId, data])
 );
+
+export const getEntriesSummary = async () => db.manyOrNone(getAllApplicantsByBatchId);
